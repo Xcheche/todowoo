6268c211d11fac0a1769a16a34f7b516eb3afc81
Ebuka Omenife <105078715+Xcheche@users.noreply.github.com>
2024-03-24 15:39:28 +0100

    signup/signin/logout-authentication implemented

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 
 from todo import views
-
+# auth
 urlpatterns = [
     path("admin/", admin.site.urls),
-# Auth
     path('signup/', views.signupuser, name='signupuser'),
-#  Todos
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    # todo
+    path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
 ]
