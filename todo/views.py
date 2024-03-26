a0c11194f8e37c39812bbd167682f75a5e196c17
Ebuka Omenife <105078715+Xcheche@users.noreply.github.com>
2024-03-26 17:59:25 +0100

    listing-todo

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .models import Todo
+from todo import forms
 
 from .forms import TodoForm
 
@@ -20,7 +22,7 @@ def signupuser(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
-                request.POST['username'], password=request.POST['password1'])
+                    request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('currenttodos')
@@ -35,19 +37,20 @@ def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
         else:
             login(request, user)
             return redirect('currenttodos')
-        
+
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
-    
+
 
 def createtodo(request):
     if request.method == 'GET':
@@ -61,7 +64,9 @@ def createtodo(request):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data passed in. Try again.'})
-    
+
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
